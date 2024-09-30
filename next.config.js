9e8f25de4e4993f75711cb3f5f33c64416bd2bf3
MartinRubio92 <martin.l.rubio92@gmail.com>
2024-09-30 15:30:46 -0300

    export creado

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/MartinRubio92.github.io',
 }
 
 module.exports = nextConfig
